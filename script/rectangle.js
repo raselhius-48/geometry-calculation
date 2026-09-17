f3ac3ffd8a4a905b7894bab4671bd9f629384f74
Rasel Hossain <raselhius@gmail.com>
2026-09-17 23:09:43 +0600

    added rectangle get length and get width

--- script/rectangle.js
+++ script/rectangle.js
@@ -1,5 +1,25 @@
 // console.log('conected rectangle')
 
 function CalculaterectangleArea() {
-    console.log('conected onclick')
+    // console.log('conected onclick')
+    // get rectangle lenght value
+    const RectangleLength = document.getElementById('rectangle-len');
+    // console.log(RectangleInput)
+    const RectangleText = RectangleLength.value;
+    // console.log(RectangleText)
+    const length = parseFloat(RectangleText);
+    console.log(length)
+
+
+
+    // get rectangle width value
+    const RectangleWidth = document.getElementById('rectangle-wid')
+    // console.log(RectangleWidth)
+    const RectangleTextWid = RectangleWidth.value
+    // console.log(RectangleTextWid)
+    const width = parseFloat(RectangleTextWid)
+    console.log(width)
+
+
+
 }
