6852d67bff6bb82f5d10b33f6f7eaab0c4ab5145
Rasel Hossain <raselhius@gmail.com>
2026-09-18 09:16:29 +0600

    added parallelogram.js and updated index.html

---
+++ script/parallelogram.js
@@ -0,0 +1,30 @@
+// console.log('conected parallelogram')
+
+// function calculateParallelogram() {
+//     // console.log('click conected')
+//     const ParallelogramB = document.getElementById('Parallelogram-b')
+//     // console.log(ParallelogramB)
+//     const ParallelogramValue = ParallelogramB.value
+//     // console.log(ParallelogramValue)
+//     const B =parseFloat(ParallelogramValue)
+
+
+
+// }
+
+
+function calculateParallelogramArea() {
+    const base = getInputValueById('Parallelogram-b')
+    console.log(base)
+    const width = getInputValueById('Parallelogram-wid')
+    console.log(width)
+
+}
+
+function getInputValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId)
+    const InputValueText = inputField.value
+    const inputValue = parseFloat(InputValueText)
+    // console.log(inputValue)
+    return inputValue
+}
