17903a4076dd44711b8eaadaacfe7d790a5fc9f6
Rasel Hossain <raselhius@gmail.com>
2026-09-18 23:04:32 +0600

    html input field and rhomnus.js conected

--- script/rhombus.js
+++ script/rhombus.js
@@ -1,9 +1,13 @@
 // console.log('conected 6')
 
 
-//  get Parallelogram value of d1 and d2
+//  get rhombus value of d1 and d2
 function calculateRhombusArea() {
     console.log('button conected')
+    const base1 = getInputvalueById('rhombus-d2')
+    console.log(base1)
+    console.log(getInputvalueById('rhombus-d1'))
+
 
 }
 
@@ -11,4 +15,5 @@ function getInputvalueById(inputId) {
     const inputfiled = document.getElementById(inputId)
     const inputValue = inputfiled.value
     const inputparse = parseFloat(inputValue)
+    return inputparse
 }
