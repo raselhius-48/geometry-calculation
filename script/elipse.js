73b2e696811d0ea29e5c715ab2d4e240b2a6f816
Rasel Hossain <raselhius@gmail.com>
2026-09-19 21:35:18 +0600

    Added ellipse.js file get ellipse value of a b

--- script/elipse.js
+++ script/elipse.js
@@ -1,1 +1,32 @@
-console.log("conected Ellipse")
+// console.log("conected Ellipse")
+
+// console.log('conected Pentagon')
+
+
+
+//  get ellipse value of a and b
+function calculateElliposeArea() {
+    // console.log('button conected')
+    const p = getInputvalueById('Ellipse-a')
+    // console.log(d1)
+    const b = getInputvalueById('Ellipse-b')
+
+    //  calculation area 
+    const ellipseArea = 0.5 * 3.1416 * p * b
+    console.log('ellipseArea = ', ellipseArea)
+
+    setInnerTextById('Ellipse-Span', area)
+}
+
+function getInputvalueById(inputId) {
+    const inputfiled = document.getElementById(inputId)
+    const inputValue = inputfiled.value
+    const inputparse = parseFloat(inputValue)
+    return inputparse
+}
+
+// calculate area display show function
+function setInnerTextById(elementId, area) {
+    const EllipseSpan = document.getElementById(elementId)
+    EllipseSpan.innerHTML = area
+}
